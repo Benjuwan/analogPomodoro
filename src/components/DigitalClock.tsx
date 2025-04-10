2be98635729f4b504de4d323ff6f0f0f14697309
benjuwan <benjuwan.banquet12@gmail.com>
2025-04-10 21:53:14 +0900

    fix: デジタル時計UIのサイズ感を修正.

--- src/components/DigitalClock.tsx
+++ src/components/DigitalClock.tsx
@@ -1,3 +1,3 @@
 export const DigitalClock = ({ currTime }: { currTime: string }) => {
-    return <section className="digitalClock absolute m-auto inset-[0] transform-[translate(0%,6em)] w-fit h-fit text-[1rem] py-[.25em] px-[1em] text-[#818181] border-[3px] border-[#818181] rounded">{currTime}</section>;
+    return <section className="digitalClock absolute m-auto inset-[0] transform-[translate(0%,6em)] w-fit h-fit text-[#818181] text-[clamp(0.75rem,calc(100vw/40),1rem)] py-[.25em] px-[1em] border-[3px] border-[#818181] rounded">{currTime}</section>;
 }
